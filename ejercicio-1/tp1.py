d20c1e5c7e2400e04916b83c09b8b3c456150e45
alkchofa <maxi-cantarell@hotmail.com>
2017-04-01 00:07:54 -0300

    Paso la primer version en Python sin objetos a Python con objetos para quedar lo mas parecido a el ejemplo en crystal posible y comparar sus tiempos

--- ejercicio-1/tp1.py
+++ ejercicio-1/tp1.py
@@ -1,135 +1,137 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
+class Tp1:
 
-HighValue=99999
-TF=240  #Tiempo para Finalizar (en minutos)
-T=0     #Tiempo
-TPLL=0  #Tiempo para proxima llegada
-NS=0    #Numero de personas en el sistema (Cola+atendiendo)
-STO=0   #Sumatoria del tiempo ocioso
-ITO=0   #Intervalo del tiempo ocioso
-SumatoriaTiempoAtencion=0  #Sumatoria del tiempo de atencion
-STLL=0  #Sumatoria del tiempo de llegada
-STS=0   #Sumatoria del tiempo de salida
-NT=0    #Personas que pasaron por el sistema
-SumatoriaArriboCon5Personas=0
-SumatoriaArrepentimientos=0
-TPS=HighValue   #Tiempo para proxima salida
-PPS=0   #Promedio de permanencia en el sistema
-PromedioEsperaEnCola=0
-PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila=0
-Sigo = True
-colaIA=[9, 11, 61, 30, 120, 20, 10]
-colaTA=[13, 10, 80, 40, 20, 30, 30]
+	def __init__(self):
+		self.HighValue=99999
+		self.TF=240  #Tiempo para Finalizar (en minutos)
+		self.T=0     #Tiempo
+		self.TPLL=0  #Tiempo para proxima llegada
+		self.NS=0    #Numero de personas en el sistema (Cola+atendiendo)
+		self.STO=0   #Sumatoria del tiempo ocioso
+		self.ITO=0   #Intervalo del tiempo ocioso
+		self.SumatoriaTiempoAtencion=0  #Sumatoria del tiempo de atencion
+		self.STLL=0  #Sumatoria del tiempo de llegada
+		self.STS=0   #Sumatoria del tiempo de salida
+		self.NT=0    #Personas que pasaron por el sistema
+		self.SumatoriaArriboCon5Personas=0
+		self.SumatoriaArrepentimientos=0
+		self.TPS=self.HighValue   #Tiempo para proxima salida
+		self.PPS=0   #Promedio de permanencia en el sistema
+		self.PromedioEsperaEnCola=0
+		self.PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila=0
+		self.Sigo = True
+		self.colaIA=[9, 11, 61, 30, 120, 20, 10]
+		self.colaTA=[13, 10, 80, 40, 20, 30, 30]
+	
+	def imprimirVariables(self,Evento):
+	    print "|-Evento= "+Evento
+	    print "|-  T   |  NS  |  NT  | TPLL |  TPS | STLL |  STS |  PPS |  ITO |  STO |  TF "
+	    print "|-"+str(self.T).center(6," ")+"|"+str(self.NS).center(6," ")+"|"+str(self.NT).center(6," ")+"|"+str(self.TPLL).center(6," ")+"|"+str(self.TPS).center(6," ")+"|"+str(self.STLL).center(6," ")+"|"+str(self.STS).center(6," ")+"|"+str(self.PPS).center(6," ")+"|"+str(self.ITO).center(6," ")+"|"+str(self.STO).center(6," ")+"|"+str(self.TF).center(6," ")
+	    print "-----------------------------------------------------------------------------"
+	
+	def dameIntervaloDeArribo(self):
+		#return random.randint(0,10)
+		#return input("Ingrese IA: ")
+	    return self.colaIA.pop()
+	
+	def dameTiempoAtencion(self):
+		#random.seed
+		#return random.randint(10,20)
+		#return input("Ingrese TA: ")
+	    return self.colaTA.pop()
+	
+	def arrepentimiento(self):
+		if self.NS < 5:
+			return False
+		else:
+			if self.NS > 8:
+				self.SumatoriaArrepentimientos += 1
+				return True
+			else:
+				if rand <= 0.4:
+					return False
+				else:
+					if self.NS == 6: #Esto es igual a 6, porque NS es la suma de la persona a la que atienen mas la de la cola
+						self.SumatoriaArriboCon5Personas += 1
+					self.SumatoriaArrepentimientos += 1
+				return True
+	
+	def obtenerResultado(self):
+		if self.NT:
+			self.PPS = (self.STS-self.STLL) / self.NT
+			self.PromedioEsperaEnCola = (self.STS-self.STLL-self.SumatoriaTiempoAtencion) / self.NT
+		else:
+			self.PPS=0
+			self.PromedioEsperaEnCola=0
+		if self.T:
+			self.PTO = (self.STO * 100) / self.T
+		else:
+			self.PTO=0
+		if self.SumatoriaArrepentimientos > 0:
+			self.PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila = (self.SumatoriaArriboCon5Personas*100 / self.SumatoriaArrepentimientos)
+		else:
+			self.PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila = 0.00
+		self.Sigo=False
 
+	def procesarLlegada(self):
+		self.T=self.TPLL
+		self.TPLL = self.T+self.dameIntervaloDeArribo()
+		if not self.arrepentimiento():
+			self.NS += 1
+			if self.NS == 1:
+				self.STO += self.T - self.ITO
+				tiempoAtencion = self.dameTiempoAtencion()
+				self.TPS = self.T + tiempoAtencion
+				self.SumatoriaTiempoAtencion += tiempoAtencion
+			self.STLL += self.T
 
-def ImprimirVariables(Evento):
-    global T, NS, NT, TPLL, TPS, STLL, STS, PPS, ITO, STO, TF
-    print "|-Evento= "+Evento
-    print "|-  T   |  NS  |  NT  | TPLL |  TPS | STLL |  STS |  PPS |  ITO |  STO |  TF "
-    print "|-"+str(T).center(6," ")+"|"+str(NS).center(6," ")+"|"+str(NT).center(6," ")+"|"+str(TPLL).center(6," ")+"|"+str(TPS).center(6," ")+"|"+str(STLL).center(6," ")+"|"+str(STS).center(6," ")+"|"+str(PPS).center(6," ")+"|"+str(ITO).center(6," ")+"|"+str(STO).center(6," ")+"|"+str(TF).center(6," ")
-    print "-----------------------------------------------------------------------------"
+	def procesarSalida(self):
+		self.T = self.TPS
+		self.NS -= 1
+		if self.NS==0:
+			self.vaciamiento=False
+		if self.NS >= 1:
+			tiempoAtencion = self.dameTiempoAtencion()
+			self.TPS = self.T + tiempoAtencion
+			self.SumatoriaTiempoAtencion += tiempoAtencion
+		else:
+			self.TPS = self.HighValue
+			self.ITO = self.T
+		self.STS += self.T
+		self.NT += 1
 
-def DameIntervaloDeArribo():
-    '''random.seed
-    return random.randint(0,10)'''
-    #return input("Ingrese IA: ")
-    return colaIA.pop()
+	def mostrarResultados(self):
+		print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		print "Promedio permanencia en el sistema = " + str(self.PPS)
+		print "Promedio espera en cola = " + str(self.PromedioEsperaEnCola)
+		print "Sumatoria tiempo ocioso= "+str(self.STO)
+		print "Porcentaje de tiempo ocioso = " + "{0:.2f}".format(self.PTO)+"%"
+		print "Porcentaje de personas arrepentidas con 5 personas en la cola = " + "{0:.2f}".format(self.PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila)+"%"
+		print "Cantidad de personas que pasaron por el sistema = "+str(self.NT)
+		print "Cantidad de Arrepentidos = "+str(self.SumatoriaArrepentimientos)
+		print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	
+	def correrSimulacion(self):
+		self.vaciamiento=False
+		while self.Sigo:
+			if self.T <= self.TF or self.vaciamiento:
+				self.imprimirVariables("Antes de entrar")
+				if self.TPLL <= self.TPS:
+					self.procesarLlegada()
+				else:
+					self.procesarSalida()
+			else:
+				if self.NS >0:
+					self.TPLL=self.HighValue
+					self.vaciamiento=True
+				else:
+					self.obtenerResultado()
+					self.mostrarResultados()
+	
+tp1 = Tp1()
 
-def DameTiempoAtencion():
-    '''random.seed
-    return random.randint(10,20)'''
-    #return input("Ingrese TA: ")
-    return colaTA.pop()
-
-def Arrepentimiento(NS):
-    random.seed()
-    global SumatoriaArrepentimientos
-    global SumatoriaArriboCon5Personas
-    if NS < 5:
-        return False
-    else:
-        if NS > 8:
-            SumatoriaArrepentimientos = SumatoriaArrepentimientos + 1
-            return True
-        else:
-            ValorRandom = random.uniform(0,1.0)
-            if ValorRandom <= 0.4:
-                return False
-            else:
-            	if NS == 6: #Esto es igual a 6, porque NS es la suma de la persona a la que atienen mas la de la cola
-            		SumatoriaArriboCon5Personas = SumatoriaArriboCon5Personas + 1
-                SumatoriaArrepentimientos = SumatoriaArrepentimientos + 1
-                return True
-
-def procesarSimulacion():
-    global PPS,PromedioEsperaEnCola,PTO,SumatoriaTiempoAtencion
-    if NT:
-        PPS = (STS-STLL) / float(NT)
-        PromedioEsperaEnCola = (STS-STLL-SumatoriaTiempoAtencion) / float(NT)
-    else:
-        PPS=0
-        PromedioEsperaEnCola=0
-    if T:
-        PTO = (STO * 100) / float(T)
-    else:
-        PTO=0
-
-Vaciamiento=False
-while Sigo:
-    if T <= TF or Vaciamiento:
-    	ImprimirVariables("Principio")
-        if TPLL <= TPS:
-            T=TPLL
-            IntervaloArribo = DameIntervaloDeArribo() #Aca deberia generar el intervalo segun la tabla
-            TPLL = T+IntervaloArribo
-            if not Arrepentimiento(NS):
-                NS = NS + 1
-                #ImprimirVariables("Llegada")
-                if NS == 1:
-                    STO = STO + (T - ITO)
-                    TiempoAtencion = DameTiempoAtencion() #Aca deberia generar el intervalo segun la tabla
-                    TPS = T + TiempoAtencion
-                    SumatoriaTiempoAtencion = SumatoriaTiempoAtencion + TiempoAtencion
-                STLL = STLL + T
-        else:
-            T = TPS
-            NS = NS - 1
-            #ImprimirVariables("Salida")
-            if NS==0:
-            	Vaciamiento=False
-            if NS >= 1:
-                TiempoAtencion = DameTiempoAtencion()
-                TPS = T + TiempoAtencion
-                SumatoriaTiempoAtencion = SumatoriaTiempoAtencion + TiempoAtencion
-            else:
-                TPS = HighValue
-                ITO = T
-            STS = STS + T
-            NT = NT + 1
-    else:
-	    if NS > 0:
-	        TPLL = HighValue
-	        Vaciamiento=True
-	    else:
-	        PPS = (STS - STLL) / NT
-	        PromedioEsperaEnCola = (STS - STLL - SumatoriaTiempoAtencion) / NT
-	        PTO = (STO * 100) / float(T)
-	        if SumatoriaArrepentimientos > 0:
-	            PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila = (SumatoriaArriboCon5Personas*100 / float(SumatoriaArrepentimientos))
-	        else:
-	            PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila = 0.00
-	        Sigo = False
-
-print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-print "Promedio permanencia en el sistema = " + str(PPS)
-print "Promedio espera en cola = " + str(PromedioEsperaEnCola)
-print "Sumatoria tiempo ocioso= "+str(STO)
-print "Porcentaje de tiempo ocioso = " + "{0:.2f}".format(PTO)+"%"
-print "Porcentaje de personas arrepentidas con 5 personas en la cola = " + "{0:.2f}".format(PorcentajePersonasQueSEFueronPorMasDe5PersonasEnLaFila)+"%"
-print "Cantidad de personas que pasaron por el sistema = "+str(NT)
-print "Cantidad de Arrepentidos = "+str(SumatoriaArrepentimientos)
-print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-ImprimirVariables("Final")
+tp1.imprimirVariables("Inicio")
+tp1.correrSimulacion()
+tp1.imprimirVariables("Fin")
